7f2a1f25e33d0e9b1a8ee82a760ea6024f3cae2d
jiaxuanlee <jiaxuan.lee.18@gmail.com>
2021-12-12 16:42:43 +0800

    update

---
+++ resources/views/addCategory.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+@section('content')
+<div class="row">
+    <div class="col-sm-3"></div>
+    <div class="col-sm-6">
+        <br><br>
+        <h3>Create New Catogory</h3>
+        <form action="">
+            <div class="form-group">
+                <label for="catogoryNamme">Category Name</label>
+                <input type="text" class="form-control" id="catogoryName" name="catogoryName">
+            </div>
+            <button type="submit" class="btn btn-primary">Add New</button>
+        </form>
+    </div>
+    <div class="col-sm-3"></div>
+
+</div>
+@endsection
