adf448323501e36e54c82aa6374eb009afd46e50
jiaxuanlee <jiaxuan.lee.18@gmail.com>
2021-12-17 10:14:32 +0800

    add to cart function

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use Session;
+use Auth;
+use App\Model\myCart;
+use App\Model\Product;
 
 class CartController extends Controller
 {
-    //
+    public function add(){
+        $r=request();
+        $addCart=myCart::Create([
+            'productID'=>$r->productID,
+            'quantity'=>$r->quantity,
+            'userID'=>Auth::id(),
+            'orderID'=>'',
+        ]);
+    }
 }
